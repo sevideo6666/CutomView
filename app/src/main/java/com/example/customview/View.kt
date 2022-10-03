b3733c68e978fb6aef61b89e66460d2ef22b3edb
Shu ya <sevideo6666@gmail.com>
2022-10-03 17:11:47 +0800

    init

---
+++ app/src/main/java/com/example/customview/View.kt
@@ -0,0 +1,60 @@
+package com.example.customview
+
+import android.animation.ValueAnimator
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+import android.os.Build
+import android.util.AttributeSet
+import android.view.View
+import android.view.animation.LinearInterpolator
+import androidx.annotation.RequiresApi
+import kotlin.properties.Delegates
+@RequiresApi(Build.VERSION_CODES.O)
+class View(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+    private var viewWith by Delegates.notNull<Float>()
+    private var viewHeight by Delegates.notNull<Float>()
+    private var currentValue = 0f
+
+    init {
+        setLayerType(LAYER_TYPE_SOFTWARE, null)
+        animator()
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val minw: Int = paddingLeft + paddingRight + suggestedMinimumWidth
+        val w: Int = resolveSizeAndState(minw, widthMeasureSpec, 1)
+        viewWith = w.toFloat()
+
+        val minh: Int = MeasureSpec.getSize(w) - rootView.width + paddingBottom + paddingTop
+        val h: Int = resolveSizeAndState(minh, heightMeasureSpec, 0)
+        viewHeight = h.toFloat()
+        setMeasuredDimension(w, h)
+    }
+
+    private fun animator(){
+        val valueAnimator = ValueAnimator.ofFloat(-1f, 1f)
+        valueAnimator.apply {
+            duration = 2000
+            repeatMode = ValueAnimator.REVERSE
+            repeatCount = ValueAnimator.INFINITE
+            interpolator = LinearInterpolator()
+            addUpdateListener {
+//                currentValue = it.animatedValue as Float
+//                postInvalidate()
+            }
+            start()
+        }
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+    }
+
+
+
+}
